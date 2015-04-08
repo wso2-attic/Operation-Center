f129c786039a5a0cf89f0af00f44da221807831a
Chanaka Cooray <chanakas@wso2.com>
2015-04-08 18:06:09 +0530

    Web App Artifact Extractor initial commit

---
+++ WSO2OperationsCenterArtifactExtractors/WSO2OperationsCenterWebAppArtifactExtractor/src/main/java/org/wso2/carbon/oc/webapp/extractor/OCWebAppExtractorConstants.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.oc.webapp.extractor;
+
+public class OCWebAppExtractorConstants {
+
+	public static final String ARTIFACT_TYPE = "webapp";
+	public static final String HOLDER_LIST_PROPERTY = "carbon.webapps.holderlist";
+	public static final String WEB_APPS = "webapps";
+	public static final String WEB_APP_FILTER = "webappFilter";
+
+	public static final String CONTEXT = "context";
+	public static final String DISPLAY_NAME = "displayName";
+	public static final String WEB_APP_TYPE = "webapptype";
+	public static final String HOST_NAME = "hostName";
+	public static final String STATE = "state";
+
+	private OCWebAppExtractorConstants(){
+
+	}
+}
